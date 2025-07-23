5da9554f771139de2168fe6b990ad36831e7cac8
Gideon <deonjo@gmail.com>
2025-07-23 15:37:23 -0700

    fixed blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import content from 'astro-content';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [content()],
+  integrations: [],
 });
